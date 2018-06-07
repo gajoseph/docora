69a3674060b2131342b3d635a859927973da6e43
George @ Joseph <georgeantonytj@hotmail.com>
2018-06-07 15:52:58 -0500

    Update runsasgrid.sh

--- runsasgrid.sh
+++ runsasgrid.sh
@@ -16,3 +16,32 @@ chmod g+rwx $ORA_PRO
 #############################################################################
 mkdir $HOME/tmp
 export TEMP=$HOME/tmp
+
+$ORA_MOUNT/grid/runInstaller -silent -noconfig  \
+-showProgress -ignoresysprereqs \
+ORACLE_HOSTNAME=server.domain.com  \
+INVENTORY_LOCATION=/u01/app/oraInventory \
+SELECTED_LANGUAGES=en \
+oracle.install.option=HA_CONFIG \
+ORACLE_BASE=$ORA_LOC \
+ORACLE_HOME=$ORA_PRO/12.1.0/grid \
+oracle.install.asm.OSDBA=asmdba \
+oracle.install.asm.OSOPER=asmoper \
+oracle.install.asm.OSASM=asmadmin \
+oracle.install.asm.SYSASMPassword=oracle_4U \
+oracle.install.asm.diskGroup.name=DATA \
+oracle.install.asm.diskGroup.redundancy=EXTERNAL \
+oracle.install.asm.diskGroup.AUSize=4 \
+oracle.install.asm.diskGroup.disks=/dev/oracleasm/disks/ORA \
+oracle.install.asm.diskGroup.diskDiscoveryString=/dev/oracleasm/disks/* \
+oracle.install.asm.monitorPassword=oracle_4U
+
+
+
+############################################################################## 
+### create vi cfgrsp.properties
+##############################################################################
+oracle.assistants.asm|S_ASMPASSWORD=oracle_4U
+oracle.assistants.asm|S_ASMMONITORPASSWORD=oracle_4U
+
+$ORACLE_HOME/grid/crs/install/roothas.pl  -deconfig -force
